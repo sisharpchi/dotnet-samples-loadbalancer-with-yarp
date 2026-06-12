f7ed8448b68dad5e7bd1f5ed61d2cb6a00302f68
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2026-06-12 11:36:15 +0500

    feat: domain entity

---
+++ MarketplaceSample/src/MarketplaceSample.Domain/Common/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace MarketplaceSample.Domain.Common;
+
+public class BaseEntity 
+{
+    public long Id { get; set; }
+}
---
+++ MarketplaceSample/src/MarketplaceSample.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using MarketplaceSample.Domain.Common;
+
+namespace MarketplaceSample.Domain.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string? ImageUrl { get; set; }
+}
